0c8abeb80eab093a77bf0396d20291a2590420cb
Bimmr <Bimmr6696@gmail.com>
2025-03-09 21:13:51 -0400

    Bow: Fixed changing weapons while having "All arrow types" active

--- autorun/Buffer/Modules/Bow.lua
+++ autorun/Buffer/Modules/Bow.lua
@@ -7,7 +7,8 @@ local Module = {
         unlimited_bottles = false,
         max_trick_arrow_gauge = false
     },
-    old = {}
+    old = {},
+    hidden = {}
 }
 
 function Module.init()
@@ -19,6 +20,34 @@ function Module.init()
 end
 
 function Module.init_hooks()
+
+    -- Watch for weapon changes, need to re-apply the default arrow types 
+    sdk.hook(sdk.find_type_definition("app.HunterCharacter"):get_method("changeWeapon"), function(args) 
+        
+        local managed = sdk.to_managed_object(args[2])
+        if not managed:get_type_definition():is_a("app.HunterCharacter") then return end
+        if not managed:get_IsMaster() then return end
+        if not managed:get_WeaponHandling() then return end
+        if not managed:get_WeaponHandling():get_type_definition() then return end
+        local is_current_bow = managed:get_WeaponHandling():get_type_definition():is_a("app.cHunterWp11Handling")
+        -- local new_weapon_id = sdk.to_int64(args[3])
+        
+        if not is_current_bow then return end
+
+        if Module.data.all_arrow_types and Module.old.arrow_types then
+
+            local weapon = managed:get_WeaponHandling()
+
+            for i = 1, #weapon:get_field("<BottleInfos>k__BackingField") do
+                local bottle_info = weapon:get_field("<BottleInfos>k__BackingField")[i]
+                if bottle_info then
+                    bottle_info:set_field("<CanLoading>k__BackingField", Module.old.arrow_types[i])
+                end
+            end
+            Module.old.arrow_types = nil
+            
+        end
+    end, function(retval) end)
     
     -- Weapon changes
     sdk.hook(sdk.find_type_definition("app.cHunterWp11Handling"):get_method("update"), function(args) 
@@ -30,28 +59,35 @@ function Module.init_hooks()
         -- Charge Level
         if Module.data.charge_level ~= -1 then
             managed:set_field("<ChargeLv>k__BackingField", Module.data.charge_level)
-            -- Could also use charge time, but does skills change this?
-            -- managed:set_field("_ChargeTimer", 2) -- 2 = level 3
         end
 
+
         -- All arrow types
-        if Module.data.all_arrow_types and not Module.old.arrow_types then
-            if not Module.old.arrow_types then
-                Module.old.arrow_types = {}
-                for i = 1, #managed:get_field("<BottleInfos>k__BackingField") do
-                    local bottle_info = managed:get_field("<BottleInfos>k__BackingField")[i]
-                    if bottle_info then
-                        Module.old.arrow_types[i] = bottle_info:get_field("<CanLoading>k__BackingField")
-                        bottle_info:set_field("<CanLoading>k__BackingField", true)
-                    end
+        if Module.data.all_arrow_types and Module.old.arrow_types == nil then
+            Module.old.arrow_types = {}
+            local bottle_infos = managed:get_field("<BottleInfos>k__BackingField")
+            for i, bottle_info in ipairs(bottle_infos) do
+                Module.old.arrow_types[i] = bottle_info:get_field("<CanLoading>k__BackingField")
+                bottle_info:set_field("<CanLoading>k__BackingField", true)
+            end
+        elseif Module.data.all_arrow_types and Module.old.arrow_types then
+            -- check if any canloading is false, if so then it didn't set and we need to re-apply and re-save (The weapon change applies twice when changing weapons)
+            local bottle_infos = managed:get_field("<BottleInfos>k__BackingField")
+            local needs_reapply = false
+            for i, bottle_info in ipairs(bottle_infos) do
+                if not bottle_info:get_field("<CanLoading>k__BackingField") then
+                    needs_reapply = true
+                    break
                 end
             end
-        elseif not Module.data.all_arrow_types and Module.old.arrow_types then
-            for i = 1, #managed:get_field("<BottleInfos>k__BackingField") do
-                local bottle_info = managed:get_field("<BottleInfos>k__BackingField")[i]
-                if bottle_info then
-                    bottle_info:set_field("<CanLoading>k__BackingField", Module.old.arrow_types[i])
-                end
+            if needs_reapply then
+                Module.old.arrow_types = nil
+            end
+        end 
+        if not Module.data.all_arrow_types and Module.old.arrow_types then
+            local bottle_infos = managed:get_field("<BottleInfos>k__BackingField")
+            for i, bottle_info in ipairs(bottle_infos) do
+                bottle_info:set_field("<CanLoading>k__BackingField", Module.old.arrow_types[i])
             end
             Module.old.arrow_types = nil
         end
