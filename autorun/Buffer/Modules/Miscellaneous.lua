fac7f0602bfea3f6843017912221b484e9bc4e37
Bimmr <Bimmr6696@gmail.com>
2025-10-02 20:52:00 -0400

    Added Pictomancy

--- autorun/Buffer/Modules/Miscellaneous.lua
+++ autorun/Buffer/Modules/Miscellaneous.lua
@@ -8,6 +8,10 @@ local Module = {
         },
         water_gun = {
             unlimited_ammo = false,
+        },
+        pictomancy = {
+            state = -1, -- 0 = Pom Motif, 2 = Wing Motif, 4 = Mog of the ages
+            instant_cooldown = false,
         }
     }
 }
@@ -29,17 +33,17 @@ function Module.init_hooks()
         if not managed:get_IsMaster() then return end    
 
         -- Akuma 
-        local ex_emote = managed:get_ExEmote00()
+        local akuma = managed:get_ExEmote00()
         -- Instant Drive Gauge
         if Module.data.akuma.instant_drive_gauge then
-            ex_emote:set_field("_aimAttackTimer", 0)
+            akuma:set_field("_aimAttackTimer", 0)
         end       
         
         -- Set Gou Hadoken to maximum level (Technically 1 (0-1))
         if Module.data.akuma.gou_hadoken_max_level then
-            if ex_emote:get_field("<ChargeLv>k__BackingField") ~= nil and ex_emote:get_field("ChargeLvMax") ~= nil then
-                local max_level = ex_emote:get_field("ChargeLvMax")
-                ex_emote:set_field("<ChargeLv>k__BackingField", max_level)
+            if akuma:get_field("<ChargeLv>k__BackingField") ~= nil and akuma:get_field("ChargeLvMax") ~= nil then
+                local max_level = akuma:get_field("ChargeLvMax")
+                akuma:set_field("<ChargeLv>k__BackingField", max_level)
             end
         end
 
@@ -48,6 +52,23 @@ function Module.init_hooks()
         local exEmote01Info = hunterInfo:get_ExEmote01Info()
         if Module.data.water_gun.unlimited_ammo then
             exEmote01Info:set_field("_AmmoCount", exEmote01Info:get_field("_MaxAmmoCount"))
+        end
+
+        -- Pictomancy
+        local pictomancy = managed:get_ExEmote03()
+        -- Instant Cooldown
+        if Module.data.pictomancy.instant_cooldown then
+            pictomancy:set_field("<PictItemCDTimer>k__BackingField", 0)
+        end
+
+        -- Pictomancy State
+        if Module.data.pictomancy.state ~= -1 then
+            local current_state = pictomancy:get_field("<PictPhase>k__BackingField")
+
+            -- Only set if different or if a multiple of 2 (0, 2, 4)
+            if current_state % 2 ~= 0 or current_state ~= Module.data.pictomancy.state * 2 then
+                pictomancy:set_field("<PictPhase>k__BackingField", Module.data.pictomancy.state * 2) -- x2 because in-game values are 0, 2, 4
+            end
         end
 
     end, function(retval)
@@ -78,6 +99,27 @@ function Module.draw()
         if imgui.tree_node(language.get(languagePrefix .. "title")) then
             
             changed, Module.data.water_gun.unlimited_ammo = imgui.checkbox(language.get(languagePrefix .. "unlimited_ammo"), Module.data.water_gun.unlimited_ammo)
+            any_changed = any_changed or changed
+
+            imgui.tree_pop()
+        end
+
+        languagePrefix = Module.title .. ".pictomancy."
+        if imgui.tree_node(language.get(languagePrefix .. "title")) then
+            languagePrefix = languagePrefix .. "state."
+            local picto_states = {
+                language.get("base.disabled"),
+                language.get(languagePrefix .. "pom"),
+                language.get(languagePrefix .. "wing"),
+                language.get(languagePrefix .. "mog")
+            }
+            local pict_state_index = Module.data.pictomancy.state + 2
+            changed, pict_state_index = imgui.combo(language.get(languagePrefix .. "title"), pict_state_index, picto_states)
+            Module.data.pictomancy.state = pict_state_index - 2
+            any_changed = any_changed or changed
+
+            languagePrefix = Module.title .. ".pictomancy."
+            changed, Module.data.pictomancy.instant_cooldown = imgui.checkbox(language.get(languagePrefix .. "instant_cooldown"), Module.data.pictomancy.instant_cooldown)
             any_changed = any_changed or changed
 
             imgui.tree_pop()
